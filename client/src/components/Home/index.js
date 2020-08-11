89f32c534fac4af971e072bf7c4399e75ea479a0
David Kavanaugh <davidkavanaugh@outlook.com>
2020-08-11 10:00:07 -0700

    fix: animation page width issue

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,96 +1,102 @@
-import React from 'react'
-import { Typography } from '@material-ui/core'
-import ReactLogo from "../../images/react-logo.png"
-import MongoLogo from '../../images/mongodb-logo.png';
-import NodeLogo from '../../images/node-logo.png';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import React from "react";
+import { Typography } from "@material-ui/core";
+import ReactLogo from "../../images/react-logo.png";
+import MongoLogo from "../../images/mongodb-logo.png";
+import NodeLogo from "../../images/node-logo.png";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 import { CSSTransition } from "react-transition-group";
-import { Link } from 'react-scroll'
-import ScrollAnimation from 'react-animate-on-scroll';
+import { Link } from "react-scroll";
+import ScrollAnimation from "react-animate-on-scroll";
 
-
-import './Home.scss'
+import "./Home.scss";
 
 export default class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            keepScrolling: false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      keepScrolling: false,
+    };
+  }
 
-    componentDidMount() {
-        window.addEventListener('scroll', this.handleScroll);
-        setTimeout(() => {
-            this.setState({
-                keepScrolling: true
-            })
-        }, 1000)
-    }
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+    setTimeout(() => {
+      this.setState({
+        keepScrolling: true,
+      });
+    }, 1000);
+  }
 
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.handleScroll);
-    }
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
 
-    handleScroll = () => {
-        this.setState({
-            keepScrolling: false
-        })
-    }
+  handleScroll = () => {
+    this.setState({
+      keepScrolling: false,
+    });
+  };
 
-    render() {
-        return (
-            <React.Fragment>
-                <div id="home">
-                    <span></span>
-                    <div>
-                        <ScrollAnimation animateIn="fadeInDown" duration={0.5}>
-                            <Typography
-                                className="title"
-                                component="h1"
-                                variant="h1"
-                                align="center"
-                            >
-                                David Kavanaugh
-                            </Typography>
-                        </ScrollAnimation>
-                        <ScrollAnimation animateIn="fadeInLeft" duration={0.5}>
-                            <Typography
-                                variant="h5"
-                                component="h5"
-                                align="center"
-                            >
-                                Full Stack Developer
-                            </Typography>
-                        </ScrollAnimation>
-                        <ScrollAnimation animateIn="fadeInRight" duration={0.5}>
-                            <div id="mern-stack">
-                                <img src={MongoLogo} alt="mongodb" />
-                                <img src={ReactLogo} alt="reactjs" />
-                                <img src={NodeLogo} alt="nodejs" />
-                            </div>
-                        </ScrollAnimation>
-                    </div>
-                    <div id="keep-scrolling">
-                        {window.pageYOffset === 0 ?
-                            <CSSTransition
-                                in={this.state.keepScrolling}
-                                appear={true}
-                                unmountOnExit
-                                classNames="keep-scrolling"
-                                timeout={500}
-                            >
-                                <div className="animate__animated animate__bounceInUp">
-                                    <Link to="about-me" spy={true} smooth={true} duration={500} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', cursor: 'pointer' }}>
-                                        keep scrolling
-                                <KeyboardArrowDownIcon />
-                                    </Link>
-                                </div>
-                            </CSSTransition>
-                            : null}
-                    </div>
+  render() {
+    return (
+      <React.Fragment>
+        <div id="home">
+          <span></span>
+          <div>
+            <ScrollAnimation animateIn="fadeInDown" duration={0.5}>
+              <Typography
+                className="title"
+                component="h1"
+                variant="h1"
+                align="center"
+              >
+                David Kavanaugh
+              </Typography>
+            </ScrollAnimation>
+            <ScrollAnimation animateIn="fadeIn" duration={0.5}>
+              <Typography variant="h5" component="h5" align="center">
+                Full Stack Developer
+              </Typography>
+            </ScrollAnimation>
+            <ScrollAnimation animateIn="fadeInUp" duration={0.5}>
+              <div id="mern-stack">
+                <img src={MongoLogo} alt="mongodb" />
+                <img src={ReactLogo} alt="reactjs" />
+                <img src={NodeLogo} alt="nodejs" />
+              </div>
+            </ScrollAnimation>
+          </div>
+          <div id="keep-scrolling">
+            {window.pageYOffset === 0 ? (
+              <CSSTransition
+                in={this.state.keepScrolling}
+                appear={true}
+                unmountOnExit
+                classNames="keep-scrolling"
+                timeout={500}
+              >
+                <div className="animate__animated animate__bounceInUp">
+                  <Link
+                    to="about-me"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      cursor: "pointer",
+                    }}
+                  >
+                    keep scrolling
+                    <KeyboardArrowDownIcon />
+                  </Link>
                 </div>
-            </React.Fragment>
-        )
-    }
+              </CSSTransition>
+            ) : null}
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }

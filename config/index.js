4468bf56f040a4317abf7902c5a633d6f5fd49c7
David Kavanaugh <davidkavanaugh@outlook.com>
2020-08-11 08:31:22 -0700

    fix: made email addresses provider-agnostic

--- config/index.js
+++ config/index.js
@@ -1,14 +1,14 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
 
-module.exports =  {
+module.exports = {
   PORT: process.env.EXPRESS_PORT,
   MONGO_URI: process.env.MONGO_URI,
   JWT: process.env.JWT,
-  OUTLOOK_PORT: process.env.OUTLOOK_PORT,
-  OUTLOOK_HOST: process.env.OUTLOOK_HOST,
-  OUTLOOK_ADDRESS: process.env.OUTLOOK_ADDRESS,
-  OUTLOOK_PASSWORD: process.env.OUTLOOK_PASSWORD,
-  OUTLOOK_USERNAME: process.env.OUTLOOK_USERNAME
+  EMAIL_PORT: process.env.EMAIL_PORT,
+  EMAIL_HOST: process.env.EMAIL_HOST,
+  EMAIL_ADDRESS: process.env.EMAIL_ADDRESS,
+  EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+  EMAIL_USERNAME: process.env.EMAIL_USERNAME,
 };
